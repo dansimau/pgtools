68f8a699468ff2723aa6316a8896eafd1cbcfbb8
Daniel Simmons <dan@dans.im>
2012-01-30 17:55:18 +0000

    Detect if node is down and handle it gracefully

--- pglag.sh
+++ pglag.sh
@@ -8,7 +8,7 @@
 
 psql="which psql"
 psql_extra_opts=""
-export PGCONNECT_TIMEOUT=5
+export PGCONNECT_TIMEOUT=3
 
 #
 # Converts specified xlog number to byte location
@@ -24,18 +24,24 @@ _get_xlog_loc()
 {
 	local pgfunc
 
-	if _in_recovery $1; then
-		pgfunc="pg_last_xlog_replay_location"
-	else
-		pgfunc="pg_current_xlog_location"
-	fi
+	if _is_up $1; then
 
-	xlog_loc=$(psql -h $1 -Atc "SELECT $pgfunc();")
-	if [ $? -gt 0 ]; then
-		echo "ERROR: Failed getting xlog location from node $1" >&2
+		if _in_recovery $1; then
+			pgfunc="pg_last_xlog_replay_location"
+		else
+			pgfunc="pg_current_xlog_location"
+		fi
+
+		xlog_loc=$(psql -h $1 -Atc "SELECT $pgfunc();")
+		if [ $? -gt 0 ]; then
+			echo "ERROR: Failed getting xlog location from node $1" >&2
+			return 1
+		fi
+		echo $xlog_loc
+		return 0
+	else
 		return 1
 	fi
-	echo $xlog_loc
 }
 
 _in_recovery()
@@ -46,7 +52,7 @@ _in_recovery()
 	recovery=$(eval echo \$$1_in_recovery)
 
 	if [ ! -n "$recovery" ]; then
-		recovery=$(psql -h $1 -Atc "SELECT pg_is_in_recovery();")
+		recovery=$(psql -h $1 -Atc "SELECT pg_is_in_recovery();" 2>/dev/null)
 
 		# Cache result
 		eval "$1_in_recovery=\"$recovery\""
@@ -62,15 +68,39 @@ _in_recovery()
 
 }
 
+_is_up()
+{
+	local state=$(eval echo \$$1_is_up)
+
+	if [ -n "$state" ]; then
+		# Return cached value
+		return $state
+	else
+		# Check if node is responding
+		psql -h $1 -Atc "SELECT 1;" &>/dev/null
+
+		# Cache result
+		eval "$1_is_up=\"$?\""
+		return $(eval echo \$$1_is_up)
+	fi
+}
+
 if [ $# -lt 1 ]; then
 	echo "Usage: $0 [--master <master host>] <slave host [...]>" >&2
 	exit 99
 fi
 
+# If a master node was manually specified at the command-line, lets try and go
+# with that (provided it's up and really is a master)
 if [ "$1" == "--master" ]; then
+
 	shift
 	master=$1
-	if _in_recovery $master; then
+
+	if ! _is_up $master; then
+		echo "WARNING: Master specified ($master) is down" >&2
+		master=""
+	elif _in_recovery $master; then
 		echo "WARNING: Master specified ($master) is not a master:" >&2
 		master=""
 	fi
@@ -99,28 +129,35 @@ echo "----------------------------------------------------"
 
 for s in "$@"; do
 
+	role=""
+	name=""
 	xlog_loc=""
 	bytes_lag=""
-	role=""
 
-	# Get xlog location
-	xlog_loc=$(_get_xlog_loc $s) || exit $?
+	name=$s
 
-	if _in_recovery $s; then
-		role="Slave  "
+	if _is_up $s; then
+
+		# Set state
+		if _in_recovery $s; then
+			role="Slave"
+		else
+			role="Master"
+		fi
+
+		# Set xlog loc
+		xlog_loc=$(_get_xlog_loc $s)
+
+		# Calculate number of bytes behind
+		if [ -n "$master" ] && [ "$s" != "$master" ]; then
+			bytes_lag=$(($(_xlog_to_bytes $master_xlog_loc) - $(_xlog_to_bytes $xlog_loc)))
+			bytes_lag="($bytes_lag bytes lag)"
+		fi
 	else
-		role="Master "
+		xlog_loc="down"
 	fi
 
-	echo -n "$role $s: $xlog_loc"
-
-	# Calculate number of bytes behind
-	if [ -n "$master" ] && [ "$s" != "$master" ]; then
-		bytes_lag=$(($(_xlog_to_bytes $master_xlog_loc) - $(_xlog_to_bytes $xlog_loc)))
-		echo -n " ($bytes_lag bytes lag)"
-	fi
-
-	echo 
+	printf "%-8s %s: %s %s\n" "$role" "$s" "$xlog_loc" "$bytes_lag"
 done
 
 echo
